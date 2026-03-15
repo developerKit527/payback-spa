82b9025cd6ff89e99e9aed8b6924bbe73adbf0e2
Chinnna <113472063+Swethapothu@users.noreply.github.com>
2026-03-15 16:41:58 -0500

    feat: add files to index.js

--- src/components/MerchantCard/index.js
+++ src/components/MerchantCard/index.js
@@ -1,1 +1,1 @@
-export { default } from './MerchantCard';
+export { default, MerchantCardSkeleton } from './MerchantCard';
--- src/components/WalletCard/index.js
+++ src/components/WalletCard/index.js
@@ -1,1 +1,1 @@
-export { default } from './WalletCard';
+export { default, WalletCardSkeleton } from './WalletCard';
